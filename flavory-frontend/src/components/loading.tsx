5004d33601613b9aef449d35272ed0b0c9f44d75
AsMa Zh <zh.asmazahani@gmail.com>
2025-11-23 19:08:13 +0100

    loading

--- flavory-frontend/src/components/loading.tsx
+++ flavory-frontend/src/components/loading.tsx
@@ -2,7 +2,7 @@
 
 export default function LoadingPage() {
     return (
-    <div className="fixed inset-0 z-50 w-full h-full flex items-center justify-center bg-beige bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/spinner-background.png')" }}>
+    <div className="absolute inset-0 z-50 w-full h-full flex items-center justify-center bg-beige bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/spinner-background.png')" }}>
       <div className="text-center">
         <h2 className="font-allura font-400 text-6xl">Flavory</h2>
         <h2 className="mt-2 font-poppins font-500 text-primary text-sm uppercase tracking-[0.3em]">crafted with Flavor</h2>
